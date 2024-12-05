1e762532044a039d94da7411b7402772121ef83e
Gareth Ellis <gareth.ellis@iconplc.com>
2024-12-05 16:47:15 +0000

    Adds skeleton for day 6 and adds run configs to VCS

---
+++ src/main/kotlin/Puzzle6.kt
@@ -0,0 +1,14 @@
+package org.garethellis.adventofcode.twentyfour
+
+class Puzzle6(inputFile: String) : Puzzle(inputFile) {
+    override val part1ExampleSolution: Int = 0
+    override val part2ExampleSolution: Int = 0
+
+    override fun part1(): Any {
+        return 0
+    }
+
+    override fun part2(): Any {
+        return 0
+    }
+}
