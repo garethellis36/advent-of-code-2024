707684ddbf423cb1b4c92fd7e322283862a8b560
Gareth Ellis <gareth.ellis@iconplc.com>
2024-12-05 10:37:33 +0000

    Changes puzzle runner so sample & real inputs are used every time

--- src/main/kotlin/Cli.kt
+++ src/main/kotlin/Cli.kt
@@ -2,14 +2,10 @@ package org.garethellis.adventofcode.twentyfour
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.options.flag
-import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.int
 
 class Cli : CliktCommand() {
     val puzzleNumber: Int by argument(help = "The puzzle (day) number of the puzzle").int()
-
-    val useSample: Boolean by option(help = "Use the sample puzzle input instead of the real input").flag(default = false)
 
     override fun run() {
         when {
@@ -32,27 +28,27 @@ class Cli : CliktCommand() {
 
     private fun runPuzzle(puzzleNumber: Int) {
 
-        var inputFile = "./input/puzzle$puzzleNumber"
-        if (useSample) {
-            inputFile += "_sample"
-        }
+        val part1SampleInputFile = "./input/puzzle${puzzleNumber}_sample"
+        val realInputFile = "./input/puzzle$puzzleNumber"
+        val part2SampleInputFile = if (puzzleNumber == 3) "./input/puzzle3_part2_sample" else part1SampleInputFile
 
-        val createPuzzle = {
+        val createPuzzle = { inputFile: String ->
             Class.forName("org.garethellis.adventofcode.twentyfour.Puzzle$puzzleNumber")
                 .getDeclaredConstructor(String::class.java)
                 .newInstance(inputFile) as Puzzle
         }
 
         echo("\n*** ADVENT OF CODE 2024 ***")
-        echo("*** Day #$puzzleNumber ***\n")
+        echo("*** Day #$puzzleNumber ***")
+        echo()
 
-        echo("Part 1 solution: ${createPuzzle().part1()}")
+        echo("Using sample input:")
+        echo("Part 1 solution: ${createPuzzle(part1SampleInputFile).part1()}")
+        echo("Part 2 solution: ${createPuzzle(part2SampleInputFile).part2()}")
+        echo()
 
-        // day 3 has different sample input for part 2
-        if (puzzleNumber == 3 && useSample) {
-            inputFile = "./input/puzzle3_part2_sample"
-        }
-
-        echo("Part 2 solution: ${createPuzzle().part2()}")
+        echo("Using real input:")
+        echo("Part 1 solution: ${createPuzzle(realInputFile).part1()}")
+        echo("Part 2 solution: ${createPuzzle(realInputFile).part2()}")
     }
 }
