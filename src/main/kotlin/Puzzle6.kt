3dae6a913bba113b036410847c84aec9be7b5b69
Gareth Ellis <gareth.ellis@iconplc.com>
2024-12-06 17:08:44 +0000

    Solves day 6:2

--- src/main/kotlin/Puzzle6.kt
+++ src/main/kotlin/Puzzle6.kt
@@ -2,9 +2,37 @@ package org.garethellis.adventofcode.twentyfour
 
 class Puzzle6(inputFile: String) : Puzzle(inputFile) {
     override val part1ExampleSolution: Int = 41
-    override val part2ExampleSolution: Int = 0
+    override val part2ExampleSolution: Int = 6
 
     override fun part1(): Any {
+        return positionsOfSafePath().count()
+    }
+
+    override fun part2(): Any {
+        val (guard, ogLab) = initLab()
+
+        val possibleLabs = positionsOfSafePath().mapIndexed { i, pos -> if (i == 0) ogLab else ogLab.addObstacle(pos) }
+
+        return possibleLabs.fold(0) { total, labWithExtraObstacle ->
+            var g = guard
+            val positions = mutableSetOf<Pair<LabPosition, GuardOrientation>>()
+
+            while (!g.hasExitedLab()) {
+                // guard has already visited this position and in this orientation, and is looping
+                if (positions.contains(Pair(g.position, g.orientation))) {
+                    return@fold total + 1
+                }
+
+                positions.add(Pair(g.position, g.orientation))
+                g = g.patrol(labWithExtraObstacle)
+            }
+
+            // guard is still able to exit the lab, adding an obstacle here did not make her loop
+            total
+        }
+    }
+
+    private fun positionsOfSafePath(): Set<LabPosition> {
         var (guard, lab) = initLab()
         val positions = mutableSetOf<LabPosition>()
 
@@ -13,14 +41,10 @@ class Puzzle6(inputFile: String) : Puzzle(inputFile) {
             guard = guard.patrol(lab)
         }
 
-        return positions.count()
+        return positions
     }
 
-    override fun part2(): Any {
-        return -1
-    }
-
-    private fun initLab(): Pair<Guard, Labaratory> {
+    private fun initLab(): Pair<Guard, Laboratory> {
         val lines = input().split('\n')
 
         var guardPosition: LabPosition? = null
@@ -38,12 +62,22 @@ class Puzzle6(inputFile: String) : Puzzle(inputFile) {
             rows.add(columns.toList())
         }
 
-        return Pair(Guard(guardPosition!!, GuardOrientation.Up), Labaratory(rows.toList()))
+        return Pair(Guard(guardPosition!!, GuardOrientation.Up), Laboratory(rows.toList()))
     }
 }
 
-class Labaratory(private val rows: List<List<LabPosition>>) {
+class Laboratory(private val rows: List<List<LabPosition>>) {
     fun has(position: LabPosition): Boolean = rows.elementAtOrNull(position.row)?.elementAtOrNull(position.col) != null
+
+    fun addObstacle(position: LabPosition): Laboratory {
+        return Laboratory(rows.mapIndexed rowMapper@{ rowIndex, cols ->
+            if (rowIndex != position.row) return@rowMapper cols
+            cols.mapIndexed colMapper@{ colIndex, pos ->
+                if (colIndex != position.col) return@colMapper pos
+                LabPosition(pos.row, pos.col, true)
+            }
+        })
+    }
 
     fun moveFrom(position: LabPosition, direction: GuardOrientation): LabPosition {
         val (nextRow, nextCol) = when (direction) {
@@ -78,8 +112,8 @@ class LabPosition(val row: Int, val col: Int, val hasObstacle: Boolean) {
     fun isInLab(): Boolean = row == -1 && col == -1
 }
 
-class Guard(val position: LabPosition, private val orientation: GuardOrientation) {
-    fun patrol(lab: Labaratory): Guard {
+class Guard(val position: LabPosition, val orientation: GuardOrientation) {
+    fun patrol(lab: Laboratory): Guard {
         val next = lab.moveFrom(position, orientation)
         return if (next.hasObstacle) Guard(position, orientation.rotateRight()) else Guard(next, orientation)
     }
