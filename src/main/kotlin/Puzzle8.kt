6fb307958df8b19cfdd54f150479817783fb75ec
Gareth Ellis <garethellis@gmail.com>
2024-12-08 18:54:53 +0000

    Adds day 8 skeleton

---
+++ src/main/kotlin/Puzzle8.kt
@@ -0,0 +1,14 @@
+package org.garethellis.adventofcode.twentyfour
+
+class Puzzle8(input: String) : Puzzle(input) {
+    override val part1ExampleSolution: Int = 14
+    override val part2ExampleSolution: Int = 0
+
+    override fun part1(): Any {
+        return -1
+    }
+
+    override fun part2(): Any {
+        return -1
+    }
+}
