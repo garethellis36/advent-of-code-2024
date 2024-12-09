49042be72ea88b1370cb6d291e7c8b3f5b8a73b6
Gareth Ellis <gareth.ellis@iconplc.com>
2024-12-09 17:02:01 +0000

    Solves 8:2

--- src/main/kotlin/Puzzle8.kt
+++ src/main/kotlin/Puzzle8.kt
@@ -8,21 +8,73 @@ typealias MapCell = Char
 
 class Puzzle8(input: String) : Puzzle(input) {
     override val part1ExampleSolution: Int = 14
-    override val part2ExampleSolution: Int = 0
+    override val part2ExampleSolution: Int = 34
 
     override fun part1(): Int {
-        val map = createMap()
+        val antennaMap = createAntennaMap()
 
-        map.effectiveAntennaPositions().forEach { (_, positions) ->
+        antennaMap.effectiveAntennaPositions().forEach { (_, positions) ->
             positions.forEachIndexed { i, a ->
                 positions.forEachIndexed { j, b ->
-                    if (j > i) map.addAntinodes(antinodePositionsFor(a, b))
+                    if (j > i) antennaMap.addAntinodes(antinodePositionsFor(a, b))
                 }
             }
         }
 
-        return map.antinodes().count()
+        return antennaMap.antinodes().count()
     }
+
+    override fun part2(): Int {
+        val antennaMap = createAntennaMap()
+
+        antennaMap.effectiveAntennaPositions().forEach { (_, positions) ->
+            positions.forEach { a ->
+                positions.forEach { b ->
+                    antinodePositionsWithResonantHarmonicsFor(a, b, antennaMap).map(antennaMap::addAntinode)
+                }
+            }
+        }
+
+        return antennaMap.antinodes().count()
+    }
+
+    private fun antinodePositionsFor(a: MapCoordinate, b: MapCoordinate): Pair<MapCoordinate, MapCoordinate> = Pair(
+        antinodePositionFor(a, b),
+        antinodePositionFor(b, a)
+    )
+
+    private fun antinodePositionFor(antenna: MapCoordinate, collaborator: MapCoordinate): MapCoordinate {
+        val (row, col) = antenna
+        val (neighbourRow, neighbourCol) = collaborator
+
+        val antinodeCoordinate = { rowOrCol: Int, neighbourRowOrCol: Int ->
+            when {
+                rowOrCol < neighbourRowOrCol -> neighbourRowOrCol + (neighbourRowOrCol - rowOrCol)
+                rowOrCol > neighbourRowOrCol -> neighbourRowOrCol - (rowOrCol - neighbourRowOrCol)
+                else -> -1
+            }
+        }
+
+        return MapCoordinate(antinodeCoordinate(row, neighbourRow), antinodeCoordinate(col, neighbourCol))
+    }
+
+    private fun antinodePositionsWithResonantHarmonicsFor(
+        antenna: MapCoordinate,
+        collaborator: MapCoordinate,
+        map: AntennaMap
+    ): List<MapCoordinate> {
+        val positions = mutableListOf(collaborator)
+
+        var antinode = antinodePositionFor(antenna, collaborator)
+        while (map.has(antinode)) {
+            positions.add(antinode)
+
+            antinode = antinodePositionFor(positions[positions.lastIndex - 1], positions.last())
+        }
+
+        return positions
+    }
+
 
     private fun draw(antinodes: Set<MapCoordinate>) {
         println(input().split('\n').mapIndexed { row, line ->
@@ -34,36 +86,7 @@ class Puzzle8(input: String) : Puzzle(input) {
         }.joinToString("\n"))
     }
 
-    private fun createMap() = AntennaMap(input().split('\n').map { line -> line.toCharArray().toList() })
-
-    override fun part2(): Int {
-        return -1
-    }
-
-    private fun antinodePositionsFor(a: MapCoordinate, b: MapCoordinate): Pair<MapCoordinate, MapCoordinate> {
-        return Pair(
-            antinodePositionFor(a, b),
-            antinodePositionFor(b, a)
-        )
-    }
-
-    private fun antinodePositionFor(pos: MapCoordinate, collaborator: MapCoordinate): MapCoordinate {
-        val (row, col) = pos
-        val (neighbourRow, neighbourCol) = collaborator
-
-        return MapCoordinate(
-            when {
-                row < neighbourRow -> neighbourRow + (neighbourRow - row)
-                row > neighbourRow -> neighbourRow - (row - neighbourRow)
-                else -> -1
-            },
-            when {
-                col < neighbourCol -> neighbourCol + (neighbourCol - col)
-                col > neighbourCol -> neighbourCol - (col - neighbourCol)
-                else -> -1
-            }
-        )
-    }
+    private fun createAntennaMap() = AntennaMap(input().split('\n').map { line -> line.toCharArray().toList() })
 }
 
 class AntennaMap(private val rows: List<MapRow>) {
@@ -89,13 +112,11 @@ class AntennaMap(private val rows: List<MapRow>) {
         addAntinode(coords.second)
     }
 
-    private fun addAntinode(coord: MapCoordinate) {
+    fun addAntinode(coord: MapCoordinate) {
         if (has(coord)) antinodes.add(coord)
     }
 
     fun antinodes() = antinodes.toSet()
 
-    private fun has(coord: MapCoordinate): Boolean {
-        return rows.elementAtOrNull(coord.first)?.elementAtOrNull(coord.second) != null
-    }
+    fun has(coord: MapCoordinate): Boolean = rows.elementAtOrNull(coord.first)?.elementAtOrNull(coord.second) != null
 }
