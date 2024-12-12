a1d0170a5c60823cbb0445edc0166838e9a8a78f
Gareth Ellis <gareth.ellis@iconplc.com>
2024-12-12 10:19:39 +0000

    Refactors day 10 to use same solution for both parts

--- src/main/kotlin/Puzzle10.kt
+++ src/main/kotlin/Puzzle10.kt
@@ -31,19 +31,18 @@ class Puzzle10(input: String) : Puzzle(input) {
 class TrailMap(private val positions: TrailMapPositions) {
     fun trailheads(): TrailheadPositions = positions.filter { entry -> entry.value == 0 }.keys
 
-    fun scoreOf(trailhead: TrailheadPosition): Int {
-        var positions = setOf(trailhead)
+    /**
+     * score = number of unique highest points you can reach from this trailhead
+     */
+    fun scoreOf(trailhead: TrailheadPosition): Int = reachableHighestPointsFrom(trailhead).toSet().count()
 
-        (0..<9).forEach { l ->
-            val newPositions = mutableSetOf<TrailheadPosition>()
-            positions.forEach { p -> nextLevels(p, l).forEach { np -> newPositions.add(np) } }
-            positions = newPositions.toSet()
-        }
+    /**
+     * rating = number of trails which reach any of the accessible highest points (i.e. some trailheads lead to the same
+     * highest point via multiple trails)
+     */
+    fun ratingOf(trailhead: TrailheadPosition): Int = reachableHighestPointsFrom(trailhead).count()
 
-        return positions.count()
-    }
-
-    fun ratingOf(trailhead: TrailheadPosition): Int {
+    private fun reachableHighestPointsFrom(trailhead: TrailheadPosition): List<TrailMapGridCoordinate> {
         var positions = listOf(trailhead)
 
         (0..<9).forEach { l ->
@@ -52,10 +51,13 @@ class TrailMap(private val positions: TrailMapPositions) {
             positions = newPositions.toList()
         }
 
-        return positions.count()
+        return positions
     }
 
-    private fun nextLevels(currentPosition: TrailMapGridCoordinate, currentLevel: Int): Set<TrailMapGridCoordinate> {
+    private fun nextLevels(
+        currentPosition: TrailMapGridCoordinate,
+        currentLevel: Int
+    ): Iterable<TrailMapGridCoordinate> {
         val (row, col) = currentPosition.split("_").map(String::toInt)
 
         val up = "${row - 1}_${col}"
@@ -65,8 +67,5 @@ class TrailMap(private val positions: TrailMapPositions) {
 
         return listOf(up, down, left, right)
             .filter { positions.contains(it) && positions[it] == currentLevel + 1 }
-            .toSet()
     }
-
-    private fun valueAt(row: Int, col: Int) = positions["${row}_${col}"]
 }
